1c4948558ed757bfaad7cb3df4c5a16d48e06840
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-01 11:57:03 +0100

    Created constants url for api

---
+++ holidaze/src/constants/api.js
@@ -0,0 +1,7 @@
+export const API_BASE_URL = 'https://api.example.com';
+
+export const VENUES_ENDPOINT = '/venues';
+export const REGISTER_ENDPOINT = 'auth/register';
+export const LOGIN_ENDPOINT = 'auth/login';
+
+
