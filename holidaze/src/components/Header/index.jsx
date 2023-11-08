eb95f46fb5fb2db2042446e59d1b0ea81a265c24
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-08 13:18:10 +0100

    Simple header structure

---
+++ holidaze/src/components/Header/index.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="">Venues</a>
+          </li>
+          <li>
+            <a href="">Account</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
