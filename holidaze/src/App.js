0199a965cdbcf3276fa085f2679828ee579ddf50
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-08 13:52:13 +0100

    Imported Layout component to main App component

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import Venues from './components/Venues'
+import Venues from './components/Venues';
+import Layout from './components/Layout';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+<Layout>
 <Venues />
+</Layout>
     </div>
   );
 }
