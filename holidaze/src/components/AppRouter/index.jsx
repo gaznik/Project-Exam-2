ebbed3ab37d1423faa7016c68a312ed38190abef
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-26 13:30:27 +0100

    Added pages in router component

--- holidaze/src/components/AppRouter/index.jsx
+++ holidaze/src/components/AppRouter/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Layout from './components/Layout';
+
+import Layout from '../components/Layout';
+import Home from '../pages/Home';
+import Profile from '../pages/Profile';
+import VenueDetail from '../pages/VenueDetail';
+import CreateVenue from '../pages/CreateVenue';
 
 function AppRouter() {
   return (
     <Router>
       <Layout>
         <Switch>
-          
+          <Route exact path="/" component={Home} />
+          <Route path="/profile" component={Profile} />
+          <Route path="/venue/:id" component={VenueDetail} />
+          <Route path="/create-venue" component={CreateVenue} />
         </Switch>
       </Layout>
     </Router>
