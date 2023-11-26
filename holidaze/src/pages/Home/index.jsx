96aa2a250a9af408fc03e9fac8d4715fcbdf5c74
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-26 17:32:41 +0100

    Basic homepage layout

---
+++ holidaze/src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Venues from '../../components/Venues';
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome to Holidaze</h2>
+      <Venues />
+    </div>
+  );
+}
+
+export default Home;
