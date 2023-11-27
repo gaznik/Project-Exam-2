db5d8e98349b61adef8d9031fe636b214b97fb2b
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-27 18:44:59 +0100

    Incorporate search comp. in venues comp.

--- holidaze/src/components/Venues/index.jsx
+++ holidaze/src/components/Venues/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { makeApiRequest } from '../../utils/api/apiRequest'; 
+import { makeApiRequest } from '../../utils/api/apiRequest';
+import Search from '../Search'; 
 
 function Venues() {
   const [venues, setVenues] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [filteredVenues, setFilteredVenues] = useState([]);
 
   useEffect(() => {
     async function fetchVenues() {
@@ -21,15 +24,27 @@ function Venues() {
     fetchVenues();
   }, []);
 
+  useEffect(() => {
+    const filtered = venues.filter((venue) =>
+      venue.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredVenues(filtered);
+  }, [searchQuery, venues]);
+
+  const handleSearch = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
   return (
     <div>
       <h1>Venues</h1>
+      <Search searchQuery={searchQuery} handleSearch={handleSearch} />
       {loading ? (
         <p>Loading...</p>
       ) : (
         <div>
           <ul>
-            {venues.map((venue) => (
+            {filteredVenues.map((venue) => (
               <li key={venue.id}>
                 <Link to={`/venues/${venue.id}`}>
                   {venue.name} - {venue.location.address}, {venue.location.city}, {venue.location.country}
