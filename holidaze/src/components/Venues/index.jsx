e26e0d316524beac3724e5fa648bf72a6849cbdf
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-27 11:13:50 +0100

    Made venues clickable

--- holidaze/src/components/Venues/index.jsx
+++ holidaze/src/components/Venues/index.jsx
@@ -1,44 +1,46 @@
 import React, { useState, useEffect } from 'react';
-import { makeApiRequest } from '../../utils/api/apiRequest';
+import { Link } from 'react-router-dom';
+import { makeApiRequest } from '../../utils/api/apiRequest'; 
 
 function Venues() {
-    const [venues, setVenues] = useState([]);
-    const [loading, setLoading] = useState(true);
-  
-    useEffect(() => {
-      async function fetchVenues() {
-        try {
-          const response = await makeApiRequest('venues');
-  
-          setVenues(response); 
-          setLoading(false);
-        } catch (error) {
-          console.error('Error fetching venues:', error);
-          setLoading(false);
-        }
+  const [venues, setVenues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchVenues() {
+      try {
+        const response = await makeApiRequest('venues');
+        setVenues(response);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching venues:', error);
+        setLoading(false);
       }
-  
-      fetchVenues();
-    }, []); 
-  
-    return (
-      <div>
-        <h1>Venues</h1>
-        {loading ? (
-          <p>Loading...</p>
-        ) : (
+    }
+
+    fetchVenues();
+  }, []);
+
+  return (
+    <div>
+      <h1>Venues</h1>
+      {loading ? (
+        <p>Loading...</p>
+      ) : (
+        <div>
           <ul>
             {venues.map((venue) => (
               <li key={venue.id}>
-              {venue.name} - {venue.location.address}, {venue.location.city}, {venue.location.country}
-            </li>
-            
+                <Link to={`/venues/${venue.id}`}>
+                  {venue.name} - {venue.location.address}, {venue.location.city}, {venue.location.country}
+                </Link>
+              </li>
             ))}
           </ul>
-        )}
-      </div>
-    );
-  }
-  
-  export default Venues;
-  
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Venues;
