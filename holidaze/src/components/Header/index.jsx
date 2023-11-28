9dbf0e09c5fb4173fa4e978df0aeb87e1a090abc
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-28 08:24:20 +0100

    Added login and register buttons to header

--- holidaze/src/components/Header/index.jsx
+++ holidaze/src/components/Header/index.jsx
@@ -12,6 +12,12 @@ function Header() {
           <li>
             <Link to="/profile">Profile</Link>
           </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
         </ul>
       </nav>
     </header>
