2ffa82deb234c4b3d7d39bd11360631da17023a5
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-29 14:48:38 +0100

    Added Search and SearchResults to homepage

--- holidaze/src/pages/Home/index.jsx
+++ holidaze/src/pages/Home/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Venues from '../../components/Venues';
+import DisplaySearchResults from '../../components/SearchResults';
+import Search from '../../components/Search';
 
 function Home() {
   return (
     <div>
       <h2>Welcome to Holidaze</h2>
+      <Search />
+      <DisplaySearchResults />
       <Venues />
     </div>
   );
