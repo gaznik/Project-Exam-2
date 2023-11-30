f6af2cbb22f2523ad1bee4da6cf63a65a47de89a
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-30 22:22:16 +0100

    Moved the venues custom hook for better structure

--- holidaze/src/components/Venues/index.jsx
+++ holidaze/src/components/Venues/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { useFetchVenues } from '../../utils/hooks/useVenueData'; 
+import { useFetchVenues } from '../../hooks/useVenueData'; 
 
 function Venues() {
   const { venues, loading, currentPage, nextPage, prevPage } = useFetchVenues();
