095a3ee1ca9886aaf65965afb42a7318c917471e
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-30 22:21:34 +0100

    Updated constants / api url

--- holidaze/src/utils/api/constants.js
+++ holidaze/src/utils/api/constants.js
@@ -1,7 +1,7 @@
 export const API_BASE_URL = 'https://api.noroff.dev/api/v1/holidaze';
 
 export const VENUES_ENDPOINT = '/venues';
-export const REGISTER_ENDPOINT = '/auth/register';
-export const LOGIN_ENDPOINT = '/auth/login';
+export const REGISTER_URL = `${API_BASE_URL}/auth/register`
+export const LOGIN_URL = `${API_BASE_URL}/auth/login`
 
 
