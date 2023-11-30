eb695fbbecb189fe9c98322dfba843569b40138c
Gazmend Nikqi <gazmendn93@gmail.com>
2023-11-30 22:19:25 +0100

    Register util for handling api post request

---
+++ holidaze/src/components/RegisterModal/registerUtil.js
@@ -0,0 +1,32 @@
+import { REGISTER_URL } from '../../utils/api/constants';
+
+async function registerUser(user) {
+    try {
+        const response = await fetch(REGISTER_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(user)
+        });
+
+        const responseData = await response.json();
+        
+        if (response.ok) {
+            const { name } = responseData;
+            console.log('Successful registration:', name);
+            
+            return { name };
+        } else {
+            console.error('Registration failed:', responseData);
+            return { success: false, error: 'That did not quite work, try again.' };
+        } 
+    } catch (error) {
+        // Log the error details
+        console.error(error);
+
+        return { success: false, error: 'That did not quite work, try again.' };
+    }
+}
+
+export default registerUser;
