197aae7c318a804639cdfbde0ba773dc34aa3393
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-02 22:10:37 +0100

    Update to auth variables

--- holidaze/src/services/api/auth/isLoggedIn.js
+++ holidaze/src/services/api/auth/isLoggedIn.js
@@ -1,4 +1,9 @@
+import { accessToken } from "../../../utils/localStorage";
+
 export function isLoggedIn() {
   const accessToken = localStorage.getItem('accessToken'); 
   return !!accessToken;
+}
+export function retrieveAccessToken() {
+  return accessToken;
 }
