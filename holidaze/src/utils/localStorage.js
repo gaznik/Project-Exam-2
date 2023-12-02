eefedb89425f7c758852f99ed9d2aaf00b536afa
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-02 10:56:30 +0100

    Retrieve items form localstorage

--- holidaze/src/utils/localStorage.js
+++ holidaze/src/utils/localStorage.js
@@ -1,3 +1,7 @@
+export const username = localStorage.getItem('username');
+export const avatar = localStorage.getItem('avatar');
+export const accessToken = localStorage.getItem('accessToken'); 
+
 export function retrieveAccessToken() {
     const accessToken = localStorage.getItem('accessToken'); 
     return accessToken;
