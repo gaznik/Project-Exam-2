4f3e149d7e9c57022c4b2d8e82ec3b6d4872c7ad
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-02 16:02:10 +0100

    Corrected endpoint in handler functions

--- holidaze/src/services/handlers/venueDetailsHandler.js
+++ holidaze/src/services/handlers/venueDetailsHandler.js
@@ -1,10 +1,10 @@
-import { makeApiRequest } from '../api/apiRequest';
-import { VENUES_ENDPOINT } from '../../utils/constants';
+import apiRequest from '../api/apiRequest';
+import { API_BASE_URL, VENUES_ENDPOINT } from '../../utils/constants';
 
 export async function fetchVenueDetails(id) {
   try {
-    const endpoint = `${VENUES_ENDPOINT}/${id}`; 
-    const response = await makeApiRequest(endpoint);
+    const endpoint = `${API_BASE_URL}${VENUES_ENDPOINT}/${id}`;
+    const response = await apiRequest(endpoint, 'GET');
     return response;
   } catch (error) {
     console.error('Error fetching venue details:', error);
--- holidaze/src/services/handlers/venuesHandler.js
+++ holidaze/src/services/handlers/venuesHandler.js
@@ -1,12 +1,11 @@
-import { makeApiRequest } from '../api/apiRequest';
-import { VENUES_ENDPOINT } from '../../utils/constants';
-
+import apiRequest from '../api/apiRequest';
+import { API_BASE_URL, VENUES_ENDPOINT } from '../../utils/constants';
 
 export async function fetchVenuesList(limit = 10, offset = 0) {
   try {
-    const endpoint = `${VENUES_ENDPOINT}?limit=${limit}&offset=${offset}`;
-    const response = await makeApiRequest(endpoint, 'GET');
-    return response; 
+    const endpoint = `${API_BASE_URL}${VENUES_ENDPOINT}?limit=${limit}&offset=${offset}`;
+    const response = await apiRequest(endpoint, 'GET');
+    return response;
   } catch (error) {
     console.error('Error fetching venues:', error);
     throw error;
