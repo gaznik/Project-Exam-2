cae3ddfa9950e3f1f624bab486241038f363d7dd
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-02 11:36:42 +0100

    Updated profile page with helmet

--- holidaze/src/pages/Profile/index.jsx
+++ holidaze/src/pages/Profile/index.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import { HelmetProvider, Helmet } from 'react-helmet-async';
+import DisplayProfile from '../../components/Profile';
 
 function Profile() {
   return (
-    <div>
-      <h1>My Profile</h1>
-    </div>
+    <HelmetProvider>
+      <div>
+        <div>
+          <Helmet>
+            <meta charset="utf-8" />
+            <link rel="icon" href="icons/loading.png" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <title>{"username"} | Holidaze</title>
+            <meta name="description" content="You can update your profile, see your bookings and venues." />
+          </Helmet>
+        </div>
+        <DisplayProfile />
+      </div>
+    </HelmetProvider>
   );
 }
 
