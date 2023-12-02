34ba23e8061b95ed124cfedc73db26a0ec5a6de4
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-02 11:33:34 +0100

    Updated constants urls

--- holidaze/src/utils/constants.js
+++ holidaze/src/utils/constants.js
@@ -1,7 +1,13 @@
+import { username } from './localStorage';
 export const API_BASE_URL = 'https://api.noroff.dev/api/v1/holidaze';
 
 export const VENUES_ENDPOINT = '/venues';
-export const REGISTER_URL = `${API_BASE_URL}/auth/register`
-export const LOGIN_URL = `${API_BASE_URL}/auth/login`
+export const REGISTER_URL = `${API_BASE_URL}/auth/register`;
+export const LOGIN_URL = `${API_BASE_URL}/auth/login`;
 
 
+export const PROFILES_URL = `${API_BASE_URL}/profiles`;
+export const PROFILE_DATA_URL = `${PROFILES_URL}/${username}`;
+export const PROFILE_MEDIA_URL = `${PROFILE_DATA_URL}/media`
+export const PROFILE_BOOKINGS_URL = `${PROFILE_DATA_URL}/bookings?_venue=true&_customer=true&sort=dateFrom&sortOrder=asc`;
+export const PROFILE_VENUES_URL = `${PROFILES_URL}/${username}/venues`;
