c87c5acf7d7f8d9de2c67d8b0f17a4f9199d6c8c
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-02 18:24:03 +0100

    Booking form

---
+++ holidaze/src/components/Booking/BookingForm.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import { useParams } from 'react-router-dom';
+import createBooking from '../../services/handlers/bookingHandler';
+import { isLoggedIn } from '../../services/api/auth/isLoggedIn';
+
+function adjustDateForTimezone(date) {
+  const tzOffset = date.getTimezoneOffset();
+  const adjustedDate = new Date(date.getTime() - tzOffset * 60000);
+  return adjustedDate.toISOString().split('T')[0];
+}
+
+function BookingForm({ selectedDates, setSelectedDates }) {
+  const { handleSubmit, control } = useForm();
+  let { id } = useParams();
+
+  const handleDateChange = (event) => {
+    const { id, value } = event.target;
+    const selectedDate = new Date(value + 'T00:00:00');
+
+    if (id === 'startDate') {
+      setSelectedDates([selectedDate, selectedDates[1]]);
+    } else if (id === 'endDate') {
+      setSelectedDates([selectedDates[0], selectedDate]);
+    }
+  };
+
+  const onSubmit = async (data) => {
+    const adjustDateForSubmission = (date) => {
+      if (!date) return null;
+      const tzOffset = date.getTimezoneOffset();
+      const adjustedDate = new Date(date.getTime() - tzOffset * 60000);
+      return adjustedDate.toISOString().split('T')[0];
+    };
+
+    const body = {
+      dateFrom: adjustDateForSubmission(selectedDates[0]),
+      dateTo: adjustDateForSubmission(selectedDates[1]),
+      guests: parseInt(data.guests),
+      venueId: id,
+    };
+
+    try {
+      const response = await createBooking(body);
+      console.log(response);
+    } catch (error) {
+      console.error('Error creating booking:', error);
+    }
+  };
+
+  return (
+    <div>
+      {!isLoggedIn() && <p>Please log in to book.</p>}
+      {isLoggedIn() && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <label htmlFor="startDate">Start Date:</label>
+          <input
+            type="date"
+            id="startDate"
+            value={selectedDates[0] ? adjustDateForTimezone(selectedDates[0]) : ''}
+            onChange={handleDateChange}
+          />
+          <label htmlFor="endDate">End Date:</label>
+          <input
+            type="date"
+            id="endDate"
+            value={selectedDates[1] ? adjustDateForTimezone(selectedDates[1]) : ''}
+            onChange={handleDateChange}
+          />
+          <div>
+            <label htmlFor="guests">Guests:</label>
+            <Controller
+              name="guests"
+              control={control}
+              defaultValue=""
+              render={({ field }) => <input {...field} type="number" />}
+            />
+          </div>
+          <div>
+            <button type="submit">Book now</button>
+          </div>
+        </form>
+      )}
+    </div>
+  );
+}
+
+export default BookingForm;
