e81658436fab1fb649988d5b1e405f0b31a36527
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-03 19:19:27 +0100

    Register/Login modal with bootstrap

--- holidaze/src/components/LoginModal/index.jsx
+++ holidaze/src/components/LoginModal/index.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import LoginForm from './LoginForm';
+import '../../styles/modal.css';
 
 function LoginModal({ onClose, onToggle }) {
   const switchToRegister = () => {
-    onClose(); 
-    onToggle(); 
+    onClose();
+    onToggle();
   };
 
   return (
-    <div>
-      <div>
-        <h1>Login</h1>
-        <LoginForm />
-        <button onClick={switchToRegister}>Register here</button>
-        <button onClick={onClose}>Close</button>
+    <div className="modal" backdrop="static" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+      <div className="modal-overlay"></div>
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Login</h5>
+            <button type="button" className="close" onClick={onClose} aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <LoginForm />
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="switch-button" onClick={switchToRegister}>Register here</button>
+          </div>
+        </div>
       </div>
     </div>
   );
--- holidaze/src/components/RegisterModal/index.jsx
+++ holidaze/src/components/RegisterModal/index.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import RegisterUserForm from './RegisterUserForm';
+import '../../styles/modal.css';
 
 function RegisterModal({ onClose, onToggle }) {
   const switchToLogin = () => {
-    onClose(); 
-    onToggle(); 
+    onClose();
+    onToggle();
   };
 
   return (
-    <div>
-      <div>
-        <h1>Registration Form</h1>
-        <RegisterUserForm />
-        <button className="switch-button" onClick={switchToLogin}>Login here</button>
-        <button className="cancel-button" onClick={onClose}>Close</button>
+    <div className="modal" tabIndex="-1" role="dialog" style={{ display: 'block' }}>
+      <div className="modal-overlay"></div>
+      <div className="modal-dialog modal-dialog-centered" role="document">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">Registration Form</h5>
+            <button type="button" className="close" onClick={onClose} aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div className="modal-body">
+            <RegisterUserForm />
+          </div>
+          <div className="modal-footer">
+            <button type="button" className="switch-button" onClick={switchToLogin}>Login here</button>
+          </div>
+        </div>
       </div>
     </div>
   );
