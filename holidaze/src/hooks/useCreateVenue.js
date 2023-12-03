6826d30ad5cb29dd807b3f8a380a04144f0888f5
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-03 01:45:18 +0100

    Custom hook for the display venue component

---
+++ holidaze/src/hooks/useCreateVenue.js
@@ -0,0 +1,51 @@
+import { useForm } from 'react-hook-form';
+import createVenue from '../services/handlers/createVenueHandler'; 
+
+function useCreateVenue() {
+  const { handleSubmit, control } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const rating = data.rating ? parseInt(data.rating) : 0;
+
+      const body = {
+        name: data.name,
+        description: data.description,
+        media: data.media.length > 0 ? [data.media] : [],
+        price: parseInt(data.price),
+        maxGuests: parseInt(data.maxGuests),
+        rating: rating,
+        meta: {
+          wifi: data.meta.wifi || false,
+          parking: data.meta.parking || false,
+          breakfast: data.meta.breakfast || false,
+          pets: data.meta.pets || false,
+        },
+        location: {
+          address: data.location.address,
+          city: data.location.city,
+          zip: data.location.zip,
+          country: data.location.country,
+          continent: data.location.continent,
+        },
+      };
+
+      const response = await createVenue(body);
+      if (response) {
+        console.log('Success');
+        // Handle success actions if needed
+      }
+    } catch (error) {
+      console.error('Something went wrong, please try again.', error);
+      // Handle error scenarios
+    }
+  };
+
+  return {
+    handleSubmit,
+    control,
+    onSubmit,
+  };
+}
+
+export default useCreateVenue;
