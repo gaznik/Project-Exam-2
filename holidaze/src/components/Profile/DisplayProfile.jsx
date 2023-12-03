c5c2c3b35cc667f18fd7fc814fa04bb3a3b41da5
Gazmend Nikqi <gazmendn93@gmail.com>
2023-12-03 10:45:28 +0100

    Changed from if statement to ternary rendering

--- holidaze/src/components/Profile/DisplayProfile.jsx
+++ holidaze/src/components/Profile/DisplayProfile.jsx
@@ -1,7 +1,7 @@
 import { PROFILE_DATA_URL } from '../../utils/constants';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import GetData from '../../services/api/useDataRetrieval'
+import GetData from '../../services/api/useDataRetrieval';
 import updateAvatar from '../../services/handlers/updateAvatarHandler';
 import MyBookings from './MyBookings';
 import BecomeVenueManager from '../../components/VenueManager/BecomeVenueManager';
@@ -11,22 +11,20 @@ function DisplayProfile() {
     const avatarPlaceholder = '/icons/user.png';
 
     const { data } = GetData(PROFILE_DATA_URL);
-    const { name, avatar, venueManager, _count } = data;
+    const { name, avatar: initialAvatar, venueManager } = data;
 
-    const [formData, setFormData] = useState({
-        avatar: '',
-    });
+    const [avatar, setAvatar] = useState(initialAvatar || '');
+    
+    useEffect(() => {
+        setAvatar(initialAvatar || '');
+    }, [initialAvatar]);
 
     const handleChange = (e) => {
-        const { name, value } = e.target;
-        setFormData({
-            ...formData,
-            [name]: value,
-        });
+        const { value } = e.target;
+        setAvatar(value);
     };
 
     const handleClick = async (e) => {
-        const avatar = formData.avatar;
         e.preventDefault();
         try {
             await updateAvatar({ avatar });
@@ -35,78 +33,48 @@ function DisplayProfile() {
         }
     };
 
-    if(_count) {
-        if(venueManager === true) {
-            return (
+    return (
+        <div>
+            <div>
+                {avatar ? <img src={avatar} alt={name} /> : <img src={avatarPlaceholder} alt={name} />}
+                <h1>{name}</h1>
+            </div>
+            <div>
+                <form>
+                    <label htmlFor='avatar'>Update avatar (url)</label>
+                    <div>
+                        <input
+                            type='url'
+                            name='avatar'
+                            value={avatar}
+                            onChange={handleChange}
+                        />
+                        <button onClick={handleClick}>Update avatar</button>
+                    </div>
+                </form>
+            </div>
+            <div>
+                <h2>My bookings</h2>
                 <div>
+                    <MyBookings />
+                </div>
+            </div>
+            {venueManager ? (
+                <div>
+                    <h2>My venues</h2>
+                    <Link to="/createVenue">Create new venue listing</Link>
                     <div>
-                        {avatar !== '' ? <img src={avatar} alt={name} /> : <img src={avatarPlaceholder} alt={name} />}
-                        <h1>{name}</h1>
-                    </div>
-                    <div>
-                        <form>
-                            <label htmlFor='avatar'>Update avatar (url)</label>
-                            <div>
-                                <input 
-                                    type='url' 
-                                    name='avatar' 
-                                    value={formData.avatar}
-                                    onChange={handleChange}
-                                />
-                                <button onClick={handleClick}>Update avatar</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div>
-                        <h2>My bookings</h2>
-                        <div>
-                            <MyBookings />
-                        </div>
-                    </div>
-                    <div>
-                        <h2>My venues</h2>
-                        <Link to="/createVenue">Create new venue listing</Link>
-                        <div>
-                            <DisplayMyVenues />
-                        </div>
+                        <DisplayMyVenues />
                     </div>
                 </div>
-            )
-        } else {
-            return (
+            ) : (
                 <div>
-                    <div>
-                        {avatar !== '' ? <img src={avatar} alt={name} /> : <img src={avatarPlaceholder} alt={name} />}
-                        <h1>{name}</h1>
-                    </div>
-                    <div>
-                        <form>
-                            <label htmlFor='avatar'>Update avatar (url)</label>
-                            <div>
-                                <input 
-                                    type='url' 
-                                    name='avatar' 
-                                    value={formData.avatar}
-                                    onChange={handleChange}
-                                />
-                                <button onClick={handleClick}>Update avatar</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div>
-                        <h2>My bookings</h2>
-                        <div>
-                            <MyBookings />                    
-                        </div>
-                    </div>
-                    <div>
-                        <p>Rent out your venue? </p>
-                        <button onClick={BecomeVenueManager} >Become a venue manager.</button>
-                    </div>
+                    <p>Rent out your venue? </p>
+                    <button onClick={BecomeVenueManager}>Become a venue manager.</button>
                 </div>
-            )
-        }
-    }
+            )}
+        </div>
+    );
 }
 
 export default DisplayProfile;
